71a4d4f9c01c17bccb4f0466cec423861fd49b93
alimardon <aboqijonov12@gmail.com>
2024-10-26 12:47:12 +0500

    added gitlab-ci.yml and Dockerfile

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-
+import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -7,10 +8,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wr@oxe3xhc1d1q@gfgaqb3lhvo0q#zkd#_l^v&i#tslx4@afhk'
+# SECRET_KEY = 'django-insecure-wr@oxe3xhc1d1q@gfgaqb3lhvo0q#zkd#_l^v&i#tslx4@afhk'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+PORT = config('PORT', default=8000, cast=int)
 
 ALLOWED_HOSTS = ['*']
 
