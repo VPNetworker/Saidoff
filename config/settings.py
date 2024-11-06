a80007454ba1817d07c2d363faf811ea4cfd3a7a
Alimardon <aboqijonov12@gmail.com>
2024-11-06 07:53:13 +0000

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -199,13 +199,16 @@ REST_FRAMEWORK = {
 
 #new settings
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://backend.saidoff.uz',
-#     'https://backend.saidoff.uz',
-#     'http://localhost:3000',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://backend.saidoff.uz',
+    'https://backend.saidoff.uz',
+    'http://localhost:3000',
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = [
+    'http://backend.saidoff.uz',
+]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
     'GET',
@@ -214,12 +217,12 @@ CORS_ALLOW_METHODS = [
     'DELETE',
     'OPTIONS',
 ]
-# CORS_ALLOW_HEADERS = [
-#     'content-type',
-#     'authorization',
-#     'x-csrf-token',
-#     'x-requested-with',
-# ]
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+    'x-csrf-token',
+    'x-requested-with',
+]
 
 
 
