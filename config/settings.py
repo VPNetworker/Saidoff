44bb1f1a83bff478ed241950bd40b9d55e5dce15
Alimardon <aboqijonov12@gmail.com>
2024-11-06 10:32:43 +0000

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -201,16 +201,16 @@ REST_FRAMEWORK = {
 
 #new settings
 
-# CORS_ALLOWED_ORIGINS = [
-    # 'http://backend.saidoff.uz',
-    # 'https://backend.saidoff.uz',
+CORS_ALLOWED_ORIGINS = [
+    'http://backend.saidoff.uz',
+    'https://backend.saidoff.uz',
 # ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = [
-    'http://backend.saidoff.uz',
-]
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+# CSRF_TRUSTED_ORIGINS = [
+    # 'http://backend.saidoff.uz',
+# ]
+# CORS_ALLOW_CREDENTIALS = True
 
 
 REST_FRAMEWORK = {
